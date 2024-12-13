2816f48eff6e06db4774cdd67b25b98c9f9eee60
henryhoangduong <henryhoangduong@gmail.com>
2024-12-13 16:12:58 +0700

    remove conflict

---
+++ src/screens/DictionaryScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const DictionaryScreen = () => {
+  return (
+    <View>
+      <Text>DictionaryScreen</Text>
+    </View>
+  );
+};
+
+export default DictionaryScreen;
+
+const styles = StyleSheet.create({});
